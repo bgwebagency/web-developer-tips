a847639fd178d3918ae677ab2d451563a80238ac
kirandash <thekirankumardash@gmail.com>
2023-05-21 20:57:40 +0800

    0002 Sort array

---
+++ 0002-sort-array/nums.ts
@@ -0,0 +1,5 @@
+const nums = [99, 33, 22, 11, -11, 0, 111]
+
+const sortedNums = nums.sort((a, b) => a - b)
+
+console.log(sortedNums)
---
+++ 0002-sort-array/strings.ts
@@ -0,0 +1,5 @@
+const names = ['kiran', 'BG Web Agency', 'Alice', 'Bob', 'Eve']
+
+const sortedNames = names.sort((a, b) => a.localeCompare(b))
+
+console.log(sortedNames)
